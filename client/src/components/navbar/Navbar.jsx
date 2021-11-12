2c4e8fd9c6cd9497f47ae3c81b58af55c60100c3
adilhijazi1980 <75063746+adilhijazi1980@users.noreply.github.com>
2021-11-12 16:58:51 +1100

    Add files via upload

---
+++ client/src/components/navbar/Navbar.jsx
@@ -0,0 +1,71 @@
+import "./navbar.css";
+import Notification from "../../img/notification.svg";
+import Message from "../../img/message.svg";
+import Settings from "../../img/settings.svg";
+import { useEffect, useState } from "react";
+
+const Navbar = ({ socket }) => {
+    const [notifications, setNotifications] = useState([]);
+    const [open, setOpen] = useState(false);
+
+    useEffect(() => {
+        socket.on("getText", (data) => {
+            setNotifications((prev) => [...prev, data]);
+        });
+    }, [socket]);
+
+    console.log(notifications);
+
+    const displayNotification = ({ senderName, text }) => {
+        // let action;
+
+        // if (type === 1) {
+        //     action = "liked";
+        // } else if (type === 2) {
+        //     action = "commented";
+        // } else {
+        //     action = "shared";
+        // }
+
+        return (
+            <span className="notification">{`${senderName}: ${text}`}</span>
+        );
+    };
+
+    const handleRead = () => {
+        setNotifications([]);
+        setOpen(false);
+    };
+
+    return (
+        <div className="navbar">
+            <span className="logo">ConstructIn App</span>
+            <div className="icons">
+                <div className="icon" onClick={() => setOpen(!open)}>
+                    <img src={Notification} alt="" className="iconImg" />
+                    {notifications.length > 0 && (
+                        <div className="counter">{notifications.length}</div>
+                    )}
+                </div>
+                <div className="icon" onClick={() => setOpen(!open)}>
+                    <img src={Message} alt="" className="iconImg" />
+                    {/* <div className="counter">2</div> */}
+                </div>
+                <div className="icon" onClick={() => setOpen(!open)}>
+                    <img src={Settings} alt="" className="iconImg" />
+                    {/* <div className="counter">2</div> */}
+                </div>
+            </div>
+            {open && (
+                <div className="notifications">
+                    {notifications.map((n) => displayNotification(n))}
+                    <button className="nButton" onClick={handleRead}>
+                        Mark as read
+                    </button>
+                </div>
+            )}
+        </div>
+    );
+};
+
+export default Navbar;
